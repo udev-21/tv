291507087c35b8e4075e00954e6d9a8b0c22c51f
Sharof Khudayberdiev <udev21@gmail.com>
2024-01-17 12:00:54 +0500

    new cameras

--- config/camera.php
+++ config/camera.php
@@ -2,11 +2,21 @@
 
 return [
     '172.16.0.134' => [
-        "entrance" => true,
-        "exit" => false,
+        "entrance" => false,
+        "exit" => true,
     ],
     '172.16.0.135' => [
         "entrance" => false,
         "exit" => true,
+    ],
+
+    '172.16.0.132' => [
+        "entrance" => true,
+        "exit" => false,
+    ],
+    
+    '172.16.0.133' => [
+        "entrance" => true,
+        "exit" => false,
     ],
 ];
